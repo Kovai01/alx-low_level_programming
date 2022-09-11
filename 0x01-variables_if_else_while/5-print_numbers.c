9f18d1554d44161e3686d1482c227cb953705739
Kovai01 <kalesanwovictor@gmail.com>
2022-09-11 13:56:06 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,10 +10,10 @@
 int main(void)
 {
 		int i;
-			for (i = 0; i < 10; i++)
-			{
-				if ("%d", i);
-			}
+		for(i = 0; i < 10; i++)
+		{
+			printf ("%d", i);
+		}
 		printf('\n');
 
 		return (0);
