7484f65acce83fc07165edc1dbb18b7e0182a9a1
Kovai01 <kalesanwovictor@gmail.com>
2022-09-12 00:12:22 -0700

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,31 +9,39 @@
 */
 int main(void)
 {
-		int i = '0';
-		int j = '0';
+		int i;
+		int j;
 
-		while (i <= '9')
+		for(i = 0; i < 10; i++)
 		{
-			while (j <= '9')
+			/**
+			 * setting the secod counter to be the value of the
+			 * previous first counter
+			 * this will ensure J doesnt go behind i no case of 10 or 31 0r 74
+			 */
+			for (j = 1; j < 10; j++)
 		{
-			if (!(i > j || i == j))
-		{
-			putchar(i);
-			putchar(j);
-		if (!(i == '8' && j == '9'))
-		{
-		putchar('\n');
+			if (i == j) /** checking if i and j are the same
+				     * e.g. 00 or 66 or 88.
+				     * if they are moved to the next iteration
+				     */
+				continue;
+			putchar(i + '0');
+			putchar(j + '0');
+
+			/**
+			 * this is to know when to print space and comma
+			 * they shouldnt be after the last value
+			 */
+			if (j == 9 && i == j - 1)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
-		else
-		{
-		putchar('.');
-		putchar (' ');
-		};
 		}
-		j++;
-		}
-		j = '0';
-		i++;
-		}
+
+		putchar('\n')
+
 		return (0);
 }
