3ae6401372f1af6c37dadd612efe3b3325e59a6c
Kovai01 <kalesanwovictor@gmail.com>
2022-09-13 15:32:17 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <cytpe.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks if the character is an alphabet
+ * @c: is the parameter used
+ * Return: if it is an alphabet then 1 if not then 0
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c) == 0)
+		return (0);
+	else
+		return (1);
+}
