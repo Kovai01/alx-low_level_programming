5f3ca786919bbb9d4d08798a9e78868ca6e228de
Kovai01 <kalesanwovictor@gmail.com>
2022-09-13 14:12:23 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char c;
+	if(c = a; c <= z; c++)
+	{
+		putchar c;
+	}
+	putchar ('\n')
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 #include <unistd.h>
 
 int _putchar(char C);
+void print_alphabet(void);
