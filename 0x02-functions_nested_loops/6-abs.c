7b51a506231fec6d58fce0c8ba3448ae491248b6
Kovai01 <kalesanwovictor@gmail.com>
2022-09-13 18:24:36 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - printing absolute values
+ * @n: number to be converted
+ * Return: absolute value of n
+ */
+int_abs(int n)
+{
+	if (n < 0)
+		n = n * -1;
+	return (n);
+}
