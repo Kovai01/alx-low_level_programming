92ef2a9772fc1ee2d6d01ae95cfdbad65964b126
Kovai01 <kalesanwovictor@gmail.com>
2022-09-14 10:23:08 -0700

    Task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,25 @@
  * Return: multiples of 3 and 5 below 1024
  */
 void find_multiples(int i)
-
-	for (int i = 1; i <= 1024; i++)
 {
-	if (i % 15 == 0)
-		printf("Multiples of 3 and 5\n");
-	else if (i % 5 == 0)
-		printf("Multiples of 5\n");
-	else if (i % 3 == 0)
-		printf("Multiples of 3\n");
-	else
-		printf("%d\n", i);
-	puts("");
+	for (int i = 1; i <= 1024; i++)
+	{
+		if (i % 15 == 0) 
+		{
+			printf("Multiples of 3 and 5\n");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Multiples of 5\n");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Multiples of 3\n");
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+	puts(" ");
+	}
 }
