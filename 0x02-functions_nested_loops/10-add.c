220937f8c3752f63f6f33fd560e92c5c9e3623da
Kovai01 <kalesanwovictor@gmail.com>
2022-09-14 03:04:49 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,16 +7,7 @@
  * @r: Result of the addition
  * Return: returns the result of the addition
  */
-int add(int n, int i, int r)
+int add(int n, int i)
 {
-	int n;
-	int i;
-	int r;
-
-	if
-		(n == 8);
-		(i == 9);
-
-		r = n + i;
-	return (r);
+	return (n + i);
 }
