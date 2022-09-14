e69b4b1fe98398e7b3f9ce2ac2c4336aa9c58858
Kovai01 <kalesanwovictor@gmail.com>
2022-09-14 02:58:20 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - add digits
+ * @n: first number to be added
+ * @i: second number to be added
+ * @r: Result of the addition
+ * Return: returns the result of the addition
+ */
+int add(int n, int i)
+{
+	int n;
+	int i;
+	int r;
+
+	if
+		(n == 8);
+		(i == 9);
+
+		r = n + i;
+	return (r);
+}
