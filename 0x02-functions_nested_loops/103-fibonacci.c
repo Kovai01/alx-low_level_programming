9bf68cb046f90d63582d756111360f96e2cac94a
Kovai01 <kalesanwovictor@gmail.com>
2022-09-14 16:56:44 -0700

    Task 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,14 +10,14 @@ int main(void)
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	float total_sum;
 
-	while(1)
+	while (1)
 	{
 		sum = fib1 + fib2;
 
-		if(sum > 4000000)
+		if (sum > 4000000)
 			break;
 
-		if((sum % 2) == 0)
+		if ((sum % 2) == 0)
 			total_sum += sum;
 
 		fib1 = fib2;
