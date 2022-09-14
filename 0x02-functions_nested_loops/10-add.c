0868c2388189370d0f94959e78a03e3e015ef8eb
Kovai01 <kalesanwovictor@gmail.com>
2022-09-14 03:05:37 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,6 @@
  * add - add digits
  * @n: first number to be added
  * @i: second number to be added
- * @r: Result of the addition
  * Return: returns the result of the addition
  */
 int add(int n, int i)
