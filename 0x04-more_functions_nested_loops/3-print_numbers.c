a3eadd12a4326c5841e305495137ffa4dbdd824f
Kovai01 <kalesanwovictor@gmail.com>
2022-09-16 04:30:38 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_numbers - check the code
+ * foloowed by a new line.
+ * Return: void
  */
-int main(void)
+void print_numbers(void)
 {
-	print_numbers();
-	return (0);
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(48 + c);
+	}
+	_putchar('\n');
 }
