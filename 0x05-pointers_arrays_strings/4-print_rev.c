add8d44ad3d532eddb284ef190c9d913981dca41
Kovai01 <kalesanwovictor@gmail.com>
2022-09-19 18:06:26 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string parameter
+ * Return: 0
+ */
+void print_rev(char *s);
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index)
+		;
+
+	for (--index; index >= 0; --index)
+		putchar(s[index]);
+	putchar('\n');
+}
