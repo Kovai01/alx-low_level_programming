4467e2a312370cdb0e7b783cd710ed7a71ba0a34
Kovai01 <kalesanwovictor@gmail.com>
2022-09-19 18:32:53 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character
+ * @str: string parameter
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		;
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
