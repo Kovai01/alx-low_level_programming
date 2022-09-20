607dee8ce01362bb7327c2c41b345a68982fba33
Kovai01 <kalesanwovictor@gmail.com>
2022-09-19 17:37:53 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ * @*str: string parameter
+ * Return: 0
+ */
+void _puts(char *str);
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
+	_putchar('\n');
+	return (counter);
+}
