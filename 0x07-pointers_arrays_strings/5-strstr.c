124c50f9462dc82e18479b91b4dc3fa8215c7e5e
Kovai01 <kalesanwovictor@gmail.com>
2022-09-26 18:55:02 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*_strstr - locates the first occurrence in the string
+* @*haystack: string targeted
+* @*needle: the recieving target
+* Return: returns a pointer to the target
+*/
+char *_strstr(char *haystack, char *needle);
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		 i = 0;
+
+		 if (haystack[1] == meedle[i])
+		{
+			do {
+				if (accept[1 + i] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		 haystack++;
+	}
+	return ('\0');
+}
