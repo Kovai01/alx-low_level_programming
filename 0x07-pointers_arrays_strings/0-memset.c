732185e8245dd7db9ed96ac9ca001b5f0ea3474e
Kovai01 <kalesanwovictor@gmail.com>
2022-09-26 17:07:08 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first byte
+ * @n: first byte to be filled
+ * @s: second byte to be filled
+ * @b: the byte to replace s
+ * Return: always 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] == b;
+		n--;
+	}
+	return (0);
+}
