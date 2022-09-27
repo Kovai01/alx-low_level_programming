777e440ccc427391ead2bd991108d3ed845f5013
Kovai01 <kalesanwovictor@gmail.com>
2022-09-26 18:02:45 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - Returns the number of bytes in the initial segment
+ * the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ * @s: initial segment
+ * @accept: cource of bytes
+ * Return: returns new value of target
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (accept[i] == *s)
+			{
+				bytes++;
+				break;
+			}
+			else if ((accept[i + 1]) == '0\')
+				return (bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
