092381701a55523eeb1f61c6a320d634dcd0c842
Kovai01 <kalesanwovictor@gmail.com>
2022-09-26 17:45:10 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_strchr - Returns a pointer to the first occurence
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+* @s: string targeted
+* @c: character targeted
+* Return: returns new value of target
+*/
+
+char *_strchr(char *s, char c)
+{
+       int i = 0;
+
+	for (i = 0; (s[i] != c) && (s[1] != '\0'); i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return(NULL);
+}
