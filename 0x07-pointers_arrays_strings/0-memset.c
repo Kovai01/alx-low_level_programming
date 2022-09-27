3a3e480a9ed8cb92f1c388260d0fb8e128eb9908
Kovai01 <111147518+Kovai01@users.noreply.github.com>
2022-09-27 01:20:56 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,21 @@
 #include "main.h"
-
 /**
- * _memset - fills the first byte
- * @n: first byte to be filled
- * @s: second byte to be filled
- * @b: the byte to replace s
- * Return: always 0
- */
+*_memset - The _memset() function fills
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	while (n)
 	{
-		s[n - 1] == b;
+		s[n - 1] = b;
 		n--;
 	}
 	return (s);
+
 }
