afbd07aa76945c3a1e442d7f2ebb04b81e4ab020
Kovai01 <kalesanwovictor@gmail.com>
2022-09-28 15:54:48 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - a function that returns the value as a square root
+ * @n: string parameter
+ * Return: natural square of a root
+ */
+int _sqrt_recursion(int n)
+{
+
+	if (n == 0 || n == 1)
+		return (n);
+	else
+		return ( _sqrt_recursion(0, n));
+}
+
+/**
+ * _sqrt - returns the square root of a number
+ * @n: test number
+ * @x: squared number
+ * Return: the natural square of a root
+ */
+
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
+}
