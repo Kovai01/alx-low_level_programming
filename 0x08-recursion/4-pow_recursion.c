ce22e17a81e8e47b4e49c8e861eaa497135e5e72
Kovai01 <kalesanwovictor@gmail.com>
2022-09-28 15:15:13 -0700

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _pow_recursion
+ * _pow_recursion - a function that returns the value as a superscript
  * @x: string parameter
  * @y: 2nd string parameter
  * Return: 0
