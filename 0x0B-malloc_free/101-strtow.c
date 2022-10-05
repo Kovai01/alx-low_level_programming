71d639701881158382c9994847db4ed049939d9e
Kovai01 <kalesanwovictor@gmail.com>
2022-10-04 20:23:45 -0700

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,1 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **strtow - splits a string into words
+ * *str: the string function
+ *
+ * Return: pointer to the array of splitted words
+ */
+char **strtow(char *str)
+{
 
