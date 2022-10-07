4c7b946d7f16dbe80bd2210ec2995c126044363d
Kovai01 <kalesanwovictor@gmail.com>
2022-10-07 07:07:03 -0700

    malloc task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - the function to multiply two positive integers
+ * return: 0
+ */
+int main(int num1, int num2)
+{
+	double product;
+	printf(num1, num2);
+	scanf("%d %d", int*, int*);
+	extern int printf (const char *__restrict __format, ...);
+
+	product = num1 * num2;
+
+	printf("Product = %.10lf", product);
+	return 0;
+}
