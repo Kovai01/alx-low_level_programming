cdce5aaa537ab0d8920f342fa69ad1fd6b567ece
Kovai01 <kalesanwovictor@gmail.com>
2022-10-12 16:53:40 -0700

    beaytiful commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: the name of the program to be written
+ * @f: another variable
+ * Return: always the variable requested
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if(name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
