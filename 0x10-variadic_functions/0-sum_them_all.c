68d31e704b961931f626e866f44a605ca52010d3
Kovai01 <kalesanwovictor@gmail.com>
2022-10-13 13:49:22 -0700

    testing my code

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,15 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - the function that sums all the variables
+ * @n: variable of int
+ * Return: always 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int test1;
+	int test2;
+	int sum = (test1 + test2);
+		if (sum == 0)
+			return (0);
+}
